6ad7f030fe21fd03036871960876491977dbaf3a
songhyunsoo <sj960126@naver.com>
2024-06-29 19:48:37 +0900

    refactor : 메인 탭바 리팩토링

--- feature_main/src/main/java/com/book/feature_main/MainScreen.kt
+++ feature_main/src/main/java/com/book/feature_main/MainScreen.kt
@@ -1,12 +1,13 @@
 package com.book.feature_main
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
@@ -14,6 +15,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.book.domain.common.entities.BookEntities
@@ -23,14 +25,13 @@ import com.book.feature_list.ListScreen
 import com.book.presentation_core.design_system.LocalColors
 import com.book.presentation_core.design_system.LocalTypography
 import com.google.gson.Gson
-import kotlinx.coroutines.InternalCoroutinesApi
 
-@ExperimentalMaterialApi
-@InternalCoroutinesApi
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
     val selectedTabIndex = rememberSaveable { mutableStateOf(TabDefine.List.ordinal) }
+    val bottomBarState = rememberSaveable { mutableStateOf(true) }
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
 
     val navigation = remember {
         object : MainNavigation {
@@ -44,25 +45,29 @@ fun MainScreen() {
             }
         }
     }
+
+    LaunchedEffect(navBackStackEntry?.destination?.route) {
+        bottomBarState.value = when (navBackStackEntry?.destination?.route) {
+            MainNavigationConst.List.route,
+            MainNavigationConst.Bookmark.route -> true
+            else -> false
+        }
+    }
+
     Scaffold(
         bottomBar = {
-            TabRow(selectedTabIndex = selectedTabIndex.value, backgroundColor = LocalColors.current.primary) {
-                TabDefine.values().forEachIndexed { index, tab ->
-                    Tab(
-                        selected = selectedTabIndex.value == index,
-                        onClick = {
-                            selectedTabIndex.value = index
-                            when (index) {
-                                TabDefine.List.ordinal -> navController.navigate(MainNavigationConst.List.route) {
-                                    popUpTo(MainNavigationConst.List.route) { inclusive = true }
-                                }
-                                TabDefine.Bookmark.ordinal -> navController.navigate(MainNavigationConst.Bookmark.route) {
-                                    popUpTo(MainNavigationConst.Bookmark.route) { inclusive = true }
-                                }
-                            } },
-                        text = { Text(text= tab.title, style = LocalTypography.current.caption2) }
-                    )
-                }
+            if (bottomBarState.value) {
+                BottomBar(selectedTabIndex = selectedTabIndex.value, onTabSelected = { index ->
+                    selectedTabIndex.value = index
+                    when (index) {
+                        TabDefine.List.ordinal -> navController.navigate(MainNavigationConst.List.route) {
+                            popUpTo(MainNavigationConst.List.route) { inclusive = true }
+                        }
+                        TabDefine.Bookmark.ordinal -> navController.navigate(MainNavigationConst.Bookmark.route) {
+                            popUpTo(MainNavigationConst.Bookmark.route) { inclusive = true }
+                        }
+                    }
+                })
             }
         }
     ) { innerPadding ->
@@ -70,8 +75,22 @@ fun MainScreen() {
             modifier = Modifier
                 .fillMaxSize()
                 .background(LocalColors.current.white)
+                .padding(innerPadding)
         ) {
-            MainScreenNavigation(navController = navController, paddingValues = innerPadding,navigation = navigation)
+            MainScreenNavigation(navController = navController, navigation = navigation)
+        }
+    }
+}
+
+@Composable
+private fun BottomBar(selectedTabIndex: Int, onTabSelected: (Int) -> Unit) {
+    TabRow(selectedTabIndex = selectedTabIndex, backgroundColor = LocalColors.current.primary) {
+        TabDefine.values().forEachIndexed { index, tab ->
+            Tab(
+                selected = selectedTabIndex == index,
+                onClick = { onTabSelected(index) },
+                text = { Text(text = tab.title, style = LocalTypography.current.caption2) }
+            )
         }
     }
 }
@@ -79,27 +98,26 @@ fun MainScreen() {
 @Composable
 private fun MainScreenNavigation(
     navController: NavHostController,
-    paddingValues: PaddingValues,
     navigation: MainNavigation
 ) {
     NavHost(
         navController = navController,
         startDestination = MainNavigationConst.List.route,
-        modifier = Modifier.padding(paddingValues)
     ) {
         composable(MainNavigationConst.List.route) {
-            ListScreen(onItemClick = {navigation.navigateToDetail(it)})
+            ListScreen(onItemClick = { navigation.navigateToDetail(it) })
         }
         composable(MainNavigationConst.Bookmark.route) {
-            BookmarkScreen(onItemClick = {navigation.navigateToDetail(it)})
+            BookmarkScreen(onItemClick = { navigation.navigateToDetail(it) })
         }
         composable(
             route = MainNavigationConst.Detail.route + "/{item}",
             arguments = listOf(navArgument("item") { type = NavType.StringType })
         ) { backStackEntry ->
             val bookDetailJson = backStackEntry.arguments?.getString("item")
-            if (bookDetailJson != null) DetailScreen(bookDetail = Gson().fromJson(bookDetailJson, BookEntities.Document::class.java))
+            if (bookDetailJson != null) {
+                DetailScreen(bookDetail = Gson().fromJson(bookDetailJson, BookEntities.Document::class.java))
+            }
         }
     }
 }
-
