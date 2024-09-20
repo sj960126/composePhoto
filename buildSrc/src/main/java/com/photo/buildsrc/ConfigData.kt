25fb60b234171ac2bba62d894a4744a1f0ea3c2d
SongHyunsoo <sj960126@naver.com>
2024-09-20 14:22:22 +0900

    chore : Kotlin Version Update

--- buildSrc/src/main/java/com/photo/buildsrc/ConfigData.kt
+++ buildSrc/src/main/java/com/photo/buildsrc/ConfigData.kt
@@ -1,8 +1,8 @@
 
 object ConfigData {
     const val nameSpace = "com.photo.photo"
-    const val compileSdkVersion = 33
-    const val targetSdkVersion = 33
+    const val compileSdkVersion = 34
+    const val targetSdkVersion = 34
     const val versionCode = 1
     const val versionName = "1.0.0"
     const val minSdkVersion = 26
--- buildSrc/src/main/java/com/photo/buildsrc/Versions.kt
+++ buildSrc/src/main/java/com/photo/buildsrc/Versions.kt
@@ -1,10 +1,10 @@
 
 object Versions {
-    const val coreKtx = "1.8.0"
+    const val coreKtx = "1.13.0"
     const val appcompat = "1.3.0"
     const val annotation = "1.1.0"
-    const val kotlinVersion = "1.6.0"
+    const val kotlinVersion = "1.8.0"
     const val hiltVersion = "2.44"
     const val room = "2.5.0"
-    const val compose = "1.2.0"
+    const val compose = "1.4.0"
 }
