76aea2ef10affd9d3ebc9342da8b0685174839f0
SongHyunsoo <sj960126@naver.com>
2024-09-25 16:58:01 +0900

    fix : 검색 아이템 안나오는 이슈 수정

--- feature/search/src/main/java/com/photo/search/SearchScreen.kt
+++ feature/search/src/main/java/com/photo/search/SearchScreen.kt
@@ -1,18 +1,13 @@
 package com.photo.search
 
-import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.flowWithLifecycle
-import androidx.paging.CombinedLoadStates
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
@@ -37,13 +32,12 @@ fun SearchScreen(
     isDualPane : Boolean,
     modifier: Modifier = Modifier
 ) {
-    val viewUiState by searchViewModel.uiState.collectAsStateWithLifecycle()
-    val context = LocalContext.current
-    val lifecycleOwner = LocalLifecycleOwner.current
+    val viewUiState by searchViewModel.uiState.collectAsState()
     val rememberCoroutineScope  = rememberCoroutineScope()
+    val context = LocalContext.current
 
-    LaunchedEffect(searchViewModel.effect,lifecycleOwner){
-        searchViewModel.effect.flowWithLifecycle(lifecycle = lifecycleOwner.lifecycle).collect { effect ->
+    LaunchedEffect(searchViewModel.effect){
+        searchViewModel.effect.collect { effect ->
             when (effect) {
                 is SearchContract.SearchSideEffect.ShowToast -> context.showToast(context.getString(effect.id))
                 is SearchContract.SearchSideEffect.MoveDetailPage -> onNavigateToDetail.invoke(effect.item)
@@ -68,109 +62,71 @@ fun SearchScreen(
                 }
             }
         )
-        SearchContent(
-            state = viewUiState.state,
-            isDualPane = isDualPane,
-            searchViewModel = searchViewModel
-        )
-    }
-}
-
-@Composable
-private fun SearchContent(
-    state : SearchContract.SearchState,
-    isDualPane: Boolean,
-    searchViewModel: SearchViewModel
-){
-    when(state){
-        SearchContract.SearchState.Init -> EmptyLayout(
-            title = stringResource(
-                id = R.string.search_empty_hint
+        when(viewUiState.state){
+            SearchContract.SearchState.Init -> EmptyLayout(
+                title = stringResource(
+                    id = R.string.search_empty_hint
+                )
             )
-        )
-        is SearchContract.SearchState.Empty -> EmptyLayout(title = state.message)
-        is SearchContract.SearchState.Error -> EmptyLayout(title = state.message)
-        is SearchContract.SearchState.Load -> {
-            SearchResult(
-                lazyPagingItems = state.item.collectAsLazyPagingItems(),
-                isDualPane = isDualPane,
-                onItemClick = {
-                    searchViewModel.handleEvent(SearchContract.SearchEvent.ClickItem(it))
-                },
-                searchViewModel = searchViewModel
-            )
+            is SearchContract.SearchState.Empty -> EmptyLayout(title = (viewUiState.state as SearchContract.SearchState.Empty).message)
+            is SearchContract.SearchState.Error -> EmptyLayout(title = (viewUiState.state as SearchContract.SearchState.Error).message)
+            is SearchContract.SearchState.Load -> {
+                val lazyPagingItems = (viewUiState.state as SearchContract.SearchState.Load).item.collectAsLazyPagingItems()
+                LoadItemsHandler(
+                    lazyPagingItems = lazyPagingItems,
+                    isDualPane = isDualPane,
+                    onItemClick = {
+                        searchViewModel.handleEvent(SearchContract.SearchEvent.ClickItem(it))
+                    },
+                    searchViewModel = searchViewModel
+                )
+            }
         }
     }
 }
-
 @Composable
-private fun SearchResult(
+private fun LoadItemsHandler(
     lazyPagingItems: LazyPagingItems<PhotoEntities.Document>,
     isDualPane: Boolean,
     onItemClick: (String) -> Unit,
-    searchViewModel: SearchViewModel) {
-    HandleLoadState(
-        loadState = lazyPagingItems.loadState,
-        searchViewModel =searchViewModel,
-        itemCount = lazyPagingItems.itemCount ?:0) {
-        LayoutSelector(
-            isDualPane = isDualPane,
-            lazyPagingItems = lazyPagingItems,
-            onItemClick = onItemClick,
-            onBookmarkClick = { (item, isBookmarked)  ->
-                searchViewModel.handleEvent(
-                    if (isBookmarked) SearchContract.SearchEvent.SaveBookmark(item)
-                    else SearchContract.SearchEvent.RemoveBookmark(item)
-                )
-            }
-        )
-    }
-}
-
-@Composable
-private fun HandleLoadState(
-    loadState: CombinedLoadStates,
-    searchViewModel: SearchViewModel,
-    itemCount : Int,
-    content: @Composable () -> Unit,
+    searchViewModel: SearchViewModel
 ) {
+    val loadState = lazyPagingItems.loadState
+
     when {
         loadState.refresh is LoadState.Error || loadState.append is LoadState.Error || loadState.prepend is LoadState.Error -> {
             searchViewModel.setEvent(
                 SearchContract.SearchEvent.ShowErrorLayout(
-                    message = stringResource(id = R.string.error_message)
+                    message = stringResource(
+                        id = R.string.error_message
+                    )
                 )
             )
         }
-        loadState.refresh is LoadState.NotLoading && itemCount == 0 -> {
-            searchViewModel.setEvent(
-                SearchContract.SearchEvent.ShowEmptyLayout(
-                    message = stringResource(id = R.string.search_empty_hint)
+        else -> {
+            if (isDualPane) {
+                DualPaneLayout(
+                    lazyPagingItems = lazyPagingItems,
+                    onItemClick = onItemClick,
+                    onBookmarkClick = {
+                        searchViewModel.handleEvent(
+                            if (it.second) SearchContract.SearchEvent.SaveBookmark(it.first)
+                            else SearchContract.SearchEvent.RemoveBookmark(it.first)
+                        )
+                    }
                 )
-            )
+            } else {
+                SinglePaneLayout(
+                    lazyPagingItems = lazyPagingItems,
+                    onItemClick = onItemClick,
+                    onBookmarkClick = {
+                        searchViewModel.handleEvent(
+                            if (it.second) SearchContract.SearchEvent.SaveBookmark(it.first)
+                            else SearchContract.SearchEvent.RemoveBookmark(it.first)
+                        )
+                    }
+                )
+            }
         }
-        else -> content()
-    }
-}
-
-@Composable
-private fun LayoutSelector(
-    isDualPane: Boolean,
-    lazyPagingItems: LazyPagingItems<PhotoEntities.Document>,
-    onItemClick: (String) -> Unit,
-    onBookmarkClick: (Pair<PhotoEntities.Document, Boolean>) -> Unit
-) {
-    if (isDualPane) {
-        DualPaneLayout(
-            lazyPagingItems = lazyPagingItems,
-            onItemClick = onItemClick,
-            onBookmarkClick = onBookmarkClick
-        )
-    } else {
-        SinglePaneLayout(
-            lazyPagingItems = lazyPagingItems,
-            onItemClick = onItemClick,
-            onBookmarkClick = onBookmarkClick
-        )
     }
 }
